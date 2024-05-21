3337402dab469b02828bc5df871edaf99308c2e9
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-05-22 01:09:27 +0300

    Almost Done

--- emailing.py
+++ emailing.py
@@ -1,5 +1,29 @@
-def Alert():
+import smtplib
+import mimetypes
+from email.message import EmailMessage
 
-    
+SENDER = "munibsmotiondetector@gmail.com"
+PASSWORD = "tfwpusgndjdapldu"
+RECIPIENT = "munibsmotiondetector@gmail.com"  # Replace with the actual recipient email address
 
-    print("Email has been sent")
+def Alert(Image):
+    Message = EmailMessage()
+    Message["Subject"] = "New customer just showed up"
+    Message.set_content("Hey, we just saw a new customer")
+
+    with open(Image, "rb") as file:
+        content = file.read()
+
+    mime_type, _ = mimetypes.guess_type(Image)
+    main_type, sub_type = mime_type.split('/')
+    Message.add_attachment(content, maintype=main_type, subtype=sub_type)
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECIPIENT, Message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    Alert(Image="images/19.png")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import time
+import glob
+import os
 from emailing import Alert
+from threading import Thread
 
 
 video = cv2.VideoCapture(0)
@@ -8,6 +11,15 @@ time.sleep(1)
 
 InitialFrame = None
 StatusList = []
+count = 1
+
+def CleanImages():
+    print("cleaning images...")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("done cleaning images...")
+
 
 while True:
 
@@ -35,13 +47,27 @@ while True:
 
         if rectangle.any():
             Status = True
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+
+            AllImages = glob.glob("images/*.png")
+            index = int(len(AllImages) / 2)
+            FinalImage = AllImages[index]
+
 
     StatusList.append(Status)
     StatusList = StatusList[-2:]
 
     if StatusList[0] == 1 and StatusList[1] == 0:
-        Alert()
-        
+        EmailThread = Thread(target=Alert, args=(FinalImage, ))
+        EmailThread.daemon = True
+        cleanThread = Thread(target=CleanImages)
+        cleanThread.daemon = True
+
+        EmailThread.start()
+        cleanThread.start()
+
+
     cv2.imshow("MyVid", frame)
     key = cv2.waitKey(1)
 
