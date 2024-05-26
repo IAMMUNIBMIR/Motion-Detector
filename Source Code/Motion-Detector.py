bd16e172caf3c7130cf3c68e539250883488246d
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-05-27 01:32:45 +0300

    test video pop up

--- Source Code/Motion-Detector.py
+++ Source Code/Motion-Detector.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import glob
 import os
+from emailing import Alert
 from threading import Thread
 from flask import Flask, render_template, request, Response
 
@@ -10,7 +11,15 @@ app = Flask(__name__)
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+InitialFrame = None
+StatusList = []
+count = 1
 activate_motion_detector = False
+
+def CleanImages():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 def motion_detection():
     global activate_motion_detector
@@ -19,8 +28,47 @@ def motion_detection():
         if not activate_motion_detector:
             continue
 
-        ret, frame = video.read()
-        cv2.imshow("MyVid", frame)
+        Status = False
+        check, frame = video.read()
+        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        grayFrameBlur = cv2.GaussianBlur(grayFrame, (21, 21), 0)
+
+        if InitialFrame is None:
+            InitialFrame = grayFrameBlur
+
+        FrameDiff = cv2.absdiff(InitialFrame, grayFrameBlur)
+        ThreshFrame = cv2.threshold(FrameDiff, 60, 255, cv2.THRESH_BINARY)[1]
+        DilatedFrame = cv2.dilate(ThreshFrame, None, iterations=2)
+
+        contours, _ = cv2.findContours(DilatedFrame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            if cv2.contourArea(contour) < 5000:
+                continue
+
+            x, y, width, height = cv2.boundingRect(contour)
+            rectangle = cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 3)
+
+            if rectangle.any():
+                Status = True
+                cv2.imwrite(f"images/{count}.png", frame)
+                count += 1
+
+                AllImages = glob.glob("images/*.png")
+                index = int(len(AllImages) / 2)
+                FinalImage = AllImages[index]
+
+        StatusList.append(Status)
+        StatusList = StatusList[-2:]
+
+        if StatusList[0] == 1 and StatusList[1] == 0:
+            EmailThread = Thread(target=Alert, args=(FinalImage,))
+            EmailThread.daemon = True
+            cleanThread = Thread(target=CleanImages)
+            cleanThread.daemon = True
+
+            EmailThread.start()
+            cleanThread.start()
 
         key = cv2.waitKey(1)
 
