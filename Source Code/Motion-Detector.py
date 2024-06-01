d56542db5c1b75c5f4093c6c5da2c94d65e89eee
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-06-02 01:09:18 +0300

    Motion detection

--- Source Code/Motion-Detector.py
+++ Source Code/Motion-Detector.py
@@ -2,17 +2,13 @@ import cv2
 import time
 import glob
 import os
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response
 from threading import Thread
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from emailing import send_email
+from emailing import Alert
 
 app = Flask(__name__, template_folder='../Website-Code')
-
-# Create directory for storing images if it doesn't exist
-if not os.path.exists('static/images'):
-    os.makedirs('static/images')
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -20,7 +16,7 @@ time.sleep(1)
 InitialFrame = None
 StatusList = []
 count = 1
-recipient_email = None  # Global variable to store recipient email
+motion_detected = False
 
 def CleanImages():
     images = glob.glob("static/images/*.png")
@@ -28,7 +24,7 @@ def CleanImages():
         os.remove(image)
 
 def generate_frames():
-    global InitialFrame, StatusList, count
+    global InitialFrame, StatusList, count, motion_detected
 
     while True:
         Status = False
@@ -38,10 +34,9 @@ def generate_frames():
 
         if InitialFrame is None:
             InitialFrame = grayFrameBlur
-            continue  # Skip processing the first frame to set the initial background
 
         FrameDiff = cv2.absdiff(InitialFrame, grayFrameBlur)
-        ThreshFrame = cv2.threshold(FrameDiff, 30, 255, cv2.THRESH_BINARY)[1]  # Adjust threshold as needed
+        ThreshFrame = cv2.threshold(FrameDiff, 60, 255, cv2.THRESH_BINARY)[1]
         DilatedFrame = cv2.dilate(ThreshFrame, None, iterations=2)
 
         contours, _ = cv2.findContours(DilatedFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -51,36 +46,43 @@ def generate_frames():
                 continue
 
             x, y, width, height = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 3)  # Draw green rectangle around detected motion
-            Status = True
+            rectangle = cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 3)
+
+            if rectangle.any():
+                Status = True
+                motion_detected = True
+                image_path = f"static/images/{count}.png"
+                cv2.imwrite(image_path, frame)
+                count += 1
+
+                AllImages = glob.glob("static/images/*.png")
+                index = int(len(AllImages) / 2)
+                FinalImage = AllImages[index]
 
         StatusList.append(Status)
         StatusList = StatusList[-2:]
 
-        if StatusList[0] == 1 and StatusList[1] == 0 and recipient_email:
-            EmailThread = Thread(target=send_email, args=(recipient_email, "static/images/{}.png".format(count)))
+        if motion_detected and StatusList[0] == 1 and StatusList[1] == 0:
+            EmailThread = Thread(target=Alert, args=(FinalImage,))
+            EmailThread.daemon = True
+            cleanThread = Thread(target=CleanImages)
+            cleanThread.daemon = True
+
             EmailThread.start()
+            cleanThread.start()
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 @app.route('/')
 def index():
-    return render_template('body.html')
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    global recipient_email
-    recipient_email = request.form.get('email')
-    print("Received email:", recipient_email)
-    return "Email submitted successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
