42b25662155990d94094afcda1809f20b11605f7
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-06-03 00:56:57 +0300

    Email receive issue new

--- Source Code/Motion-Detector.py
+++ Source Code/Motion-Detector.py
@@ -99,7 +99,7 @@ def submit_email():
 # Route for the homepage
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('./Website Code/index.html')
 
 # Run the Flask app
 if __name__ == '__main__':
