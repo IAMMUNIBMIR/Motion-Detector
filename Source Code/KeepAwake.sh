565e01c1e00b1cbe5ed095aa2e885641bc213859
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-07-19 17:39:05 +0300

    Render server fixed

--- Source Code/KeepAwake.sh
+++ Source Code/KeepAwake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL of Render server
-URL = "https://munibsmotiondetector.netlify.app/"
+URL = "https://motion-detector.onrender.com"
 
 # Send a request to the server
 curl -s $URL > /dev/null
