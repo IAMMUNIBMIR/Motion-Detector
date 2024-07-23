987c27e93d4c2ca7a5a61ffa56a05e7dc074998f
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2024-07-23 04:24:35 +0300

    Awake File Fixed

--- Source Code/KeepAwake.sh
+++ Source Code/KeepAwake.sh
@@ -5,3 +5,10 @@ URL="https://motion-detector.onrender.com"
 
 # Send a request to the server
 curl -s $URL > /dev/null
+
+# Log responses to a file in the current directory
+logfile="$(pwd)/keepawake.log"
+
+echo "Netlify response: $response_netlify" >> $logfile
+echo "Render response: $response_render" >> $logfile
+echo "KeepAwake.sh ran at $(date)" >> $logfile
